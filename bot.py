f2dae95850e2cfec25e7ef00ec0053275ae30f00
Daniel <daniel.j.fletcher2@gmail.com>
2017-06-28 15:12:22 -0700

    Slack bot responds only when mentioned or in a direct message

--- bot.py
+++ bot.py
@@ -8,10 +8,18 @@ bot_id = 'U5ZTDN8TB'
 # initialize slack client
 sc = SlackClient(slack_token)
 
-
 def is_for_me(event):
-    # TODO Implement later
-    return True
+    type = event.get('type')
+    if type and type == 'message' and not event.get('user') == bot_id:
+        return is_private(event) or im_mentioned(event)
+
+def is_private(event):
+    channel = event.get('channel')
+    return channel.startswith('D')
+
+def im_mentioned(event):
+    text = event.get('text')
+    return '<@{}>'.format(bot_id) in text.strip().split()
 
 def handle_message(message, user, channel):
     # TODO Implement later
@@ -22,17 +30,16 @@ def post_message(message, channel):
                           text=message, as_user=True)
 def run():
     if sc.rtm_connect():
-        print 'HOORAY'
+        print "Connected"
         while True:
             event_list = sc.rtm_read()
             if len(event_list) > 0:
                 for event in event_list:
-                    print event
                     if is_for_me(event):
                         handle_message(message=event.get('text'), user=event.get('user'), channel=event.get('channel'))
-            time.sleep(6)
+            time.sleep(1)
     else:
-        print('[!] Connection to Slack failed.')
+        print('Connection to Slack failed')
 
 if __name__=='__main__':
     run()
