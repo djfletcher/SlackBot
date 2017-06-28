c78794cc8ecaabf64940650a6e387d6f919c6436
Daniel <daniel.j.fletcher2@gmail.com>
2017-06-28 13:11:45 -0700

    slack bot responds in slack channel whenever a message is posted

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 from slackclient import SlackClient
+import time
 
 slack_token = 'xoxb-203931756929-eyHe3QYwJYBDmwqrUE9OhlKb'
 bot_name = 'hooray'
@@ -7,9 +8,31 @@ bot_id = 'U5ZTDN8TB'
 # initialize slack client
 sc = SlackClient(slack_token)
 
-sc.api_call(
-    'chat.postMessage',
-    channel="#general",
-    text="oodelolly oodelolly golly whatta day",
-    as_user=True
-)
+
+def is_for_me(event):
+    # TODO Implement later
+    return True
+
+def handle_message(message, user, channel):
+    # TODO Implement later
+    post_message(message=message, channel=channel)
+
+def post_message(message, channel):
+    sc.api_call('chat.postMessage', channel=channel,
+                          text=message, as_user=True)
+def run():
+    if sc.rtm_connect():
+        print 'HOORAY'
+        while True:
+            event_list = sc.rtm_read()
+            if len(event_list) > 0:
+                for event in event_list:
+                    print event
+                    if is_for_me(event):
+                        handle_message(message=event.get('text'), user=event.get('user'), channel=event.get('channel'))
+            time.sleep(6)
+    else:
+        print('[!] Connection to Slack failed.')
+
+if __name__=='__main__':
+    run()
